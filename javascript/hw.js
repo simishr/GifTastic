aefd31f68b6f0c72a59f4e325704f531c7ac55cc
Aashish Shrestha <aashishshrestha@Aashishs-MacBook-Air.local>
2016-12-22 12:14:24 -0500

    files upload

---
+++ javascript/hw.js
@@ -0,0 +1,59 @@
+var animalList = ["Dolphin", "Whale", "Shark", "Starfish", "Seahorse", "Jellyfish", "Turtle", "Crab"];
+
+
+function displayGifsAndInfos() {
+
+	var marine = $(this).attr("data-name");
+	var myKey = "&api_key=dc6zaTOxFJmzC";
+	var queryURL = "http://api.giphy.com/v1/gifs/search?limit=10&q=" + marine + myKey;
+
+	$.ajax({
+		url: queryURL,
+		method: "GET" 
+	}).done(function (response) {
+			console.log(response);
+			for(var j = 0; j < response.data.length; j++) {
+
+				var animalHolder = $("<div class='item'>");
+
+				var ratingHolder = $("<div>").addClass("ratingClass");
+				$("#animal-form").append(ratingHolder);
+				console.log(response.data[j].rating);
+				ratingHolder.html(response.data[j].rating);
+
+				var imageHolder = $("<img>").addClass("imageClass");
+				$("#animal-form").append(imageHolder);
+				console.log(response.data[j].images.fixed_height_still.url);
+				imageHolder.attr("src", response.data[j].images.fixed_height_still.url);
+				
+				var gifHolder = $("<img>").addClass("gifClass");
+				$("#animal-form").append(gifHolder);
+				console.log(response.data[j].images.fixed_height.url);
+				gifHolder.attr("src",response.data[j].images.fixed_height.url);
+			}
+		});
+}
+
+
+function renderButtons() {
+
+	 $("#marineButtons").empty();
+
+	for (var i = 0; i < animalList.length; i++) {
+		var a = $("<button>").addClass("marine-class").attr("data-name", animalList[i]).text(animalList[i]);
+		$("#marineButtons").append(a);
+	}
+}
+
+	$("#add-animal").on("click", function(event) {
+		event.preventDefault();
+		var animal = $("#animal-input").val().trim();
+		animalList.push(animal);
+		renderButtons();
+		$(gifHolder).hide();
+	});
+
+	$(document).on("click", ".marine-class", displayGifsAndInfos);
+	renderButtons();
+
+	
